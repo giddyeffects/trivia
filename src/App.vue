1838e661642607c303dc2b3196ad5125cd7dbbea
Nikiman <nikimangroup@gmail.com>
2017-08-12 23:23:25 +0300

    Initial version

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,93 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+  <div id="app" class="body">
+    <div class="sky-form" v-for="(quiz, index) in quizes" v-show="index === questionIndex">
+      <header>{{ quiz.category }}</header>
+      <h2 class="input">{{ quiz.question }}</h2>
+      <fieldset>
+        <section>
+          <div class="row">
+            <div class="col col-4">
+              <ol style="text-align:left">
+                <li v-for="answer in quiz.incorrect_answers">
+                  <label class="radio"><input type="radio" name="answer" v-model="answers[index]" :value="answer"><i></i>{{  answer }}</label>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </section>
+      </fieldset>
+      <footer v-if="questionIndex < quizes.length">
+        <button class="button" @click="next">next</button>
+        <button class="button" v-if="questionIndex > 0 " @click="prev">prev</button>
+      </footer>
+    </div>
+    <div class="sky-form">
+      <fieldset>
+        <section>
+          <span v-if="questionIndex == quizes.length"> Your Total score is {{ score }} / {{ quizes.length }}</span>
+        </section>
+      </fieldset>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+let quiz_questions = [
+  {
+    "category": "Entertainment: Film",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Who directed \'E.T. the Extra-Terrestial' (1982)?",
+    "correct_answer": "Steven Spielberg",
+    "incorrect_answers": [
+      "Steven Spielberg",
+      "Stanley Johns",
+      "Tim Burton",
+      "James Cameroon"
+    ]
+  },
+  {
+    "category": "Science & Nature",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "What is the hottest planet in the Solar System?",
+    "correct_answer": "Venus",
+    "incorrect_answers": [
+      "Venus",
+      "Mars",
+      "Mercury",
+      "Jupiter"
+    ]
+  },
+]
 
 export default {
   name: 'app',
-  components: {
-    Hello
+  data: () => {
+    return {
+      questionIndex: 0,
+      quizes: quiz_questions,
+      answers: Array(quiz_questions.length).fill(''),
+    }
+  },
+  methods: {
+    next: function() {
+      this.questionIndex++;
+    },
+    prev: function() {
+      this.questionIndex--;
+    }
+  },
+  computed: {
+    score: function() {
+      let total = 0;
+      for (let i=0; i < this.answers.length; i++) {
+        if (this.answers[i] == this.quizes[i].correct_answer) {
+          total += 1;
+        }
+      }
+      return total;
+    }
   }
 }
 </script>
